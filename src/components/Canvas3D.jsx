ae5887dee0004cfecebd70bebfe3d42d27f8b65f
jessejp <jessepernila@gmail.com>
2023-05-26 20:44:13 +0300

    3d banner, and bubbles component update

---
+++ src/components/Canvas3D.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import Can3D from "./Can3D";
+import { Stage } from "@react-three/drei";
+
+const Canvas3D = () => {
+  return (
+    <Canvas>
+      <Stage castShadow={false} shadows={false}>
+        <Can3D rotation={[-0.1, 0.0, -0.2]} />
+      </Stage>
+    </Canvas>
+  );
+};
+
+export default Canvas3D;
